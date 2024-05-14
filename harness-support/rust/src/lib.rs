00a0a4631e9cf7ec490379336810cc5465505d61
Carl Wallace <carl@redhoundsoftware.com>
2024-05-14 06:36:36 -0400

    cleanup and align with new IP address constraint support

--- harness-support/rust/src/lib.rs
+++ harness-support/rust/src/lib.rs
@@ -3,5 +3,15 @@ use models::Limbo;
 pub mod models;
 
 pub fn load_limbo() -> Limbo {
-    serde_json::from_reader(std::io::stdin()).unwrap()
+    #[cfg(not(debug_assertions))]
+    {
+        serde_json::from_reader(std::io::stdin()).unwrap()
+    }
+    #[cfg(debug_assertions)]
+    {
+        use std::fs::File;
+        use std::path::Path;
+        let f = File::open(Path::new("limbo_small.json")).unwrap();
+        serde_json::from_reader(f).unwrap()
+    }
 }
